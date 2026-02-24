c52d4d20435f43704d88da280ddd817581a820dc
Akshay Sabe <akshay,sabe11@gmail.com>
2026-02-24 12:32:35 +0530

    modified test runner file

--- src/test/java/cucumber/TestngcucumberTestRunner.java
+++ src/test/java/cucumber/TestngcucumberTestRunner.java
@@ -2,7 +2,9 @@ package cucumber;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+//link feature with stepdefination
 
+//link feature with stepdefination
 @CucumberOptions(
     features = "src/test/java/cucumber",               
     glue = "SeleniumFrameworkDesign.stepDefination",        
